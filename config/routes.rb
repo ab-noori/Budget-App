7837a398f872d6842cca418ad2a25b8f89e8ee7b
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-09-13 15:54:45 +0330

    Add routing for the user index and authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users
+  root "users#index"
 end
