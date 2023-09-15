52f39ef33922d06e76926e168fc5921cc2a007c7
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-09-15 22:14:22 +0330

    Modify the user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,24 @@
 class UsersController < ApplicationController
   def index; end
+
+  def show
+    return unless params[:id] == 'sign_out'
+
+    sign_out current_user
+    redirect_to new_user_session_path
+  end
+
+  def create
+    @user = User.new(sign_up_params)
+
+    if @user.save
+      redirect_to new_user_session_path
+    else
+      render :new
+    end
+  end
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
+  end
 end
