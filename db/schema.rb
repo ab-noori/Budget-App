99e7ca72a2dd8dff09d76df6508fbd0ed129af45
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-09-15 22:23:40 +0330

    Add the schema

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_163757) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "role"
+    t.string "name", null: false
+    t.string "role", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
